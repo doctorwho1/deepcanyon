89eba94e8125bcf46d03e424f57fed57bc17b2b1
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-18 18:01:39 +0200

    Creado proyecto Maven

---
+++ src/main/java/com/monkeys/deepcanyon/App.java
@@ -0,0 +1,13 @@
+package com.monkeys.deepcanyon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
