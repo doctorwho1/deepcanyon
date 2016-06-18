1bf94ccd796799e3283fc230a98363f84cc32ad7
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-18 19:36:19 +0200

    WaitingInQueueState: Pasados los test para los casos de cuerda vacía y cuerda llena.

--- src/main/java/com/monkeys/deepcanyon/domain/state/WaitingInQueueState.java
+++ src/main/java/com/monkeys/deepcanyon/domain/state/WaitingInQueueState.java
@@ -6,8 +6,25 @@ import com.monkeys.deepcanyon.domain.Rope;
 public class WaitingInQueueState implements MonkeyState {
 
 	public MonkeyState handle(Monkey monkey, Rope rope) {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.isRopeFull(rope)) {
+			if (this.areSameDirectionMonkeys(monkey, rope.getMonkeys().iterator().next().getMonkey())) {
+				return MonkeyStates.GIVING_WAY;
+			} else {
+				return MonkeyStates.WAITING_IN_QUEUE;
+			}
+
+		} else {
+			return MonkeyStates.TRYING_GET_ROPE;
+		}
+
+	}
+
+	public boolean areSameDirectionMonkeys(Monkey firstMonkey, Monkey secondMonkey) {
+		return firstMonkey.getCrossDirection().equals(secondMonkey.getCrossDirection());
+	}
+
+	public boolean isRopeFull(Rope rope) {
+		return rope.getLength() == rope.getMonkeys().size();
 	}
 
 }
