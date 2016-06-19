bf1d429e6293ed855813b7c9554d0530e95e9d10
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-19 10:05:03 +0200

    Cubiernos todos los casos de prueba. Caso retornar a la cola, cuando somos incapaces de coger la cuerda antes que los monos en dirección contraria.

--- src/main/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeState.java
+++ src/main/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeState.java
@@ -7,20 +7,28 @@ public class TryingGetRopeState extends BaseMonkeyState {
 
 	public MonkeyState handle(Monkey monkey, Rope rope) {
 
-		int currentMonkeyPosition;
-		switch (monkey.getCrossDirection()) {
-		case EASTWARD:
-			currentMonkeyPosition = rope.getEastwardFirstPosition();
-			break;
-		case WESTWARD:
-			currentMonkeyPosition = rope.getWestwardFirstPosition();
-			break;
-		default:
-			throw new IllegalStateException("Monkey should have a cross direction");
+		if (rope.getMonkeys().size() == 0
+				|| this.areSameDirectionMonkeys(monkey, rope.getMonkeys().iterator().next().getMonkey())) {
+			// FIXME: No estoy tratando correctamente los casos concurrentes.
+
+			int currentMonkeyPosition;
+			switch (monkey.getCrossDirection()) {
+			case EASTWARD:
+				currentMonkeyPosition = rope.getEastwardFirstPosition();
+				break;
+			case WESTWARD:
+				currentMonkeyPosition = rope.getWestwardFirstPosition();
+				break;
+			default:
+				throw new IllegalStateException("Monkey should have a cross direction");
+			}
+
+			rope.putMonkey(currentMonkeyPosition, monkey);
+			return MonkeyStateFactory.createCrossingRopeState(currentMonkeyPosition);
+		} else {
+			// Can not get rope, monkeys in opposite direction.
+			return MonkeyStateFactory.createWaitingInQueueState();
 		}
-		
-		rope.putMonkey(currentMonkeyPosition, monkey);
-		return MonkeyStateFactory.createCrossingRopeState(currentMonkeyPosition);
 	}
 
 }
