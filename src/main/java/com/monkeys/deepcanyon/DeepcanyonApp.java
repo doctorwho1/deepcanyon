5f1fb466f8d0b69f1d08f3ecedf82e4b170307e2
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-19 20:14:52 +0200

    Número aleatorio de monos creados al inicio.

--- src/main/java/com/monkeys/deepcanyon/DeepcanyonApp.java
+++ src/main/java/com/monkeys/deepcanyon/DeepcanyonApp.java
@@ -5,8 +5,10 @@ import java.util.Random;
 import com.monkeys.deepcanyon.domain.CrossDirection;
 import com.monkeys.deepcanyon.domain.Monkey;
 
-
 public class DeepcanyonApp {
+	
+	private static final int MAX_NUMBER_INITIAL_MONKEYS = 4;
+	private static final int MAX_INTERVAL_ARRIVAL_MONKEYS = 8;
 
 	private final Random random = new Random();
 
@@ -18,26 +20,34 @@ public class DeepcanyonApp {
 
 	private void run() throws InterruptedException {
 
-		while(true){
+		this.createRandonNumberOfInitialMonkeys();
+
+		while (true) {
 			Thread.sleep(this.generateRandomMonkeyWaitArrival());
 			this.wrapMonkeyInThread();
 		}
-		
-		
+
 	}
-	
-	private void wrapMonkeyInThread(){
+
+	private void createRandonNumberOfInitialMonkeys() {
+		int numberOfInitialMonkeys = random.nextInt(DeepcanyonApp.MAX_NUMBER_INITIAL_MONKEYS);
+		for (int i = 0; i < numberOfInitialMonkeys; i++) {
+			this.wrapMonkeyInThread();
+		}
+	}
+
+	private void wrapMonkeyInThread() {
 		new Thread(new MonkeyRunnable(this.generateRandomMonkey())).start();
 	}
-	
-	private long generateRandomMonkeyWaitArrival(){
-		int seconds = random.nextInt(8) + 1;
-		
-		return 1000*seconds;
+
+	private long generateRandomMonkeyWaitArrival() {
+		int seconds = random.nextInt(DeepcanyonApp.MAX_INTERVAL_ARRIVAL_MONKEYS) + 1;
+
+		return 1000 * seconds;
 	}
 
 	private Monkey generateRandomMonkey() {
-		return new Monkey(CrossDirection.values()[random.nextInt(2)]);
+		return new Monkey(CrossDirection.values()[random.nextInt(CrossDirection.values().length)]);
 	}
 
 }
