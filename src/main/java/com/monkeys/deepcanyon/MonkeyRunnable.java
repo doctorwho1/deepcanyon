188110308e973131d000202a26e9e4bee467b01d
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-19 20:04:40 +0200

    Arreglados problemas de manejo incorrecto de los hilos

--- src/main/java/com/monkeys/deepcanyon/MonkeyRunnable.java
+++ src/main/java/com/monkeys/deepcanyon/MonkeyRunnable.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MonkeyRunnable implements Runnable {
 
 	private Monkey monkey;
+	private boolean monkeyCrossed = false;
 
 	public MonkeyRunnable(Monkey monkey) {
 		super();
@@ -17,7 +18,7 @@ public class MonkeyRunnable implements Runnable {
 	}
 
 	public void run() {
-		while (true) {
+		while (!this.monkeyCrossed) {
 			MonkeyState previousState = this.monkey.getState();
 			this.monkey.think();
 
@@ -25,9 +26,8 @@ public class MonkeyRunnable implements Runnable {
 				log.info(this.monkey.toString());
 			}
 
-			if (!(this.monkey.getState() instanceof CrossedRopeState)) {
-				Thread.currentThread().interrupt();
-
+			if (this.monkey.getState() instanceof CrossedRopeState) {
+				this.monkeyCrossed = true;
 			}
 
 		}
