9d7d059358f91716909703f5fc44496e5469cf82
Ricardo Fanjul <ricardo@fintonic.com>
2016-06-19 15:19:33 +0200

    TryingGetRopeStateTestCase: Arreglados los test e implementado el comportamiento de un segundo en subirse a la cuerda.

--- src/main/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeState.java
+++ src/main/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeState.java
@@ -1,30 +1,38 @@
 package com.monkeys.deepcanyon.domain.state;
 
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+
 import com.monkeys.deepcanyon.domain.Monkey;
 import com.monkeys.deepcanyon.domain.MonkeyFactory;
 import com.monkeys.deepcanyon.domain.Rope;
 
 public class TryingGetRopeState extends BaseMonkeyState {
 
+	private DateTime initStateTime = DateTime.now();
+
 	public TryingGetRopeState(Monkey monkey, Rope rope) {
 		super(monkey, rope);
 	}
 
 	public MonkeyState handle() {
-		
+
+		MonkeyState result = this;
 		Monkey firstMonkey = this.rope.getFirstMonkey();
 
-		if (firstMonkey == null
-				|| this.areSameDirectionMonkeys(monkey, firstMonkey)) {
-
+		if (this.isElapsedOneSecond() && (firstMonkey == null || this.areSameDirectionMonkeys(monkey, firstMonkey))) {
 
 			// TODO: ¿Mover a la factoría la introducción del mono en la cuerda?
 			rope.addMonkey(monkey);
-			return MonkeyFactory.createCrossingRopeState(this.monkey);
-		} else {
-			// Can not get rope, monkeys in opposite direction.
-			return MonkeyFactory.createWaitingInQueueState(monkey);
+			result = MonkeyFactory.createCrossingRopeState(this.monkey);
 		}
+
+		return result;
+
+	}
+
+	private boolean isElapsedOneSecond() {
+		return Seconds.secondsBetween(this.initStateTime, DateTime.now()).getSeconds() >= 1;
 	}
 
 }
--- src/test/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeStateTestCase.java
+++ src/test/java/com/monkeys/deepcanyon/domain/state/TryingGetRopeStateTestCase.java
@@ -9,26 +9,25 @@ import org.junit.Test;
 import com.monkeys.deepcanyon.domain.CrossDirection;
 import com.monkeys.deepcanyon.domain.Monkey;
 import com.monkeys.deepcanyon.domain.MonkeyFactory;
-import com.monkeys.deepcanyon.domain.Rope;
-import com.monkeys.deepcanyon.util.TestUtil;
 
 public class TryingGetRopeStateTestCase extends BaseStateTestCase {
 
-
 	private DateTime currentDateTime;
-	
+
 	@Before
-	public void init(){
+	public void init() {
 		this.currentDateTime = DateTime.now();
-		DateTimeUtils.setCurrentMillisFixed(this.currentDateTime.getMillis());
+		DateTimeUtils.setCurrentMillisFixed(currentDateTime.getMillis());
 	}
-	
+
 	@Test
 	public void handleEmptyRopeEastWardDirection() {
 		Monkey monkey = new Monkey(CrossDirection.EASTWARD);
-		MonkeyState newState = MonkeyFactory.createTryingGetRopeState(monkey).handle();
+		MonkeyState monkeyState = MonkeyFactory.createTryingGetRopeState(monkey);
 
-		Assert.assertEquals(CrossingRopeState.class, newState.getClass());
+		DateTimeUtils.setCurrentMillisFixed(this.currentDateTime.plusSeconds(10000).getMillis());
+
+		Assert.assertEquals(CrossingRopeState.class, monkeyState.handle().getClass());
 
 		// TODO: Comprobar que el mono ahora ocupa la primera posición de la
 		// cuerda.
@@ -37,57 +36,31 @@ public class TryingGetRopeStateTestCase extends BaseStateTestCase {
 	@Test
 	public void handleEmptyRopeWestWardDirection() {
 		Monkey monkey = new Monkey(CrossDirection.WESTWARD);
-		MonkeyState newState = MonkeyFactory.createTryingGetRopeState(monkey).handle();
+		MonkeyState monkeyState = MonkeyFactory.createTryingGetRopeState(monkey);
 
-		Assert.assertEquals(CrossingRopeState.class, newState.getClass());
+		DateTimeUtils.setCurrentMillisFixed(this.currentDateTime.plusSeconds(10000).getMillis());
+
+		Assert.assertEquals(CrossingRopeState.class, monkeyState.handle().getClass());
 
 		// TODO: Comprobar que el mono ahora ocupa la primera posición de la
 		// cuerda.
 	}
 
 	@Test
-	public void handleEastWardMonkeysInOppositeDirection() {
-		Monkey monkey = new Monkey(CrossDirection.WESTWARD);
-		Rope rope = TestUtil.createEmptyRope();
-		rope.addMonkey(Monkey.builder().crossDirection(CrossDirection.EASTWARD).build());
+	public void testStartCrossingRopeRequireInSecondInTryingGetRope() {
 
-		MonkeyState newState = MonkeyFactory.createTryingGetRopeState(monkey).handle();
-
-		Assert.assertEquals(WaitingInQueueState.class, newState.getClass());
-
-		// TODO: Comprobar que el mono regresa a la primera posición de la cola.
-	}
-
-	@Test
-	public void handleWestWardMonkeysInOppositeDirection() {
 		Monkey monkey = new Monkey(CrossDirection.EASTWARD);
-		Rope rope = TestUtil.createEmptyRope();
-		rope.addMonkey(Monkey.builder().crossDirection(CrossDirection.WESTWARD).build());
-
-		MonkeyState newState = MonkeyFactory.createTryingGetRopeState(monkey).handle();
-
-		Assert.assertEquals(WaitingInQueueState.class, newState.getClass());
-
-		// TODO: Comprobar que el mono regresa a la primera posición de la cola.
-	}
-	
-	@Test
-	public void testStartCrossingRopeRequireInSecondInTryingGetRope(){
-		
-		Monkey monkey = new Monkey(CrossDirection.EASTWARD);
-		Rope rope = TestUtil.createEmptyRope();
-		rope.addMonkey(Monkey.builder().crossDirection(CrossDirection.WESTWARD).build());
 
 		MonkeyState monkeyState = MonkeyFactory.createTryingGetRopeState(monkey);
-		
-		
 		Assert.assertEquals(monkeyState, monkeyState.handle());
-		DateTimeUtils.setCurrentMillisFixed(this.currentDateTime.plusSeconds(1).getMillis());
-		Assert.assertEquals(WaitingInQueueState.class, monkeyState.handle());
-		
+
+		Assert.assertEquals(TryingGetRopeState.class, monkeyState.handle().getClass());
+
+		DateTimeUtils.setCurrentMillisFixed(this.currentDateTime.plusSeconds(10000).getMillis());
+
 		MonkeyState newState = monkeyState.handle();
-		Assert.assertEquals(WaitingInQueueState.class, newState.getClass());
-		
+		Assert.assertEquals(CrossingRopeState.class, newState.getClass());
+
 	}
 
 }
